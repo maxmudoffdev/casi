5d572c3af741b94a81fb20d86b346de169e4c9d0
maxmudoffdev <smaxmudov015@gmail.com>
2026-05-05 12:26:23 +0500

    create users, articles and journal apps change settings

---
+++ casi/users/admin.py
@@ -0,0 +1,18 @@
+from allauth.account.decorators import secure_admin_login
+from django.conf import settings
+from django.contrib import admin
+from django.contrib.auth import admin as auth_admin
+from django.utils.translation import gettext_lazy as _
+
+
+from .models import User
+
+if settings.DJANGO_ADMIN_FORCE_ALLAUTH:
+    # Force the `admin` sign in process to go through the `django-allauth` workflow:
+    # https://docs.allauth.org/en/latest/common/admin.html#admin
+    admin.autodiscover()
+    admin.site.login = secure_admin_login(admin.site.login)  # type: ignore[method-assign]
+
+
+admin.site.register(User)
+
